5048e3881451b6a8ae4e74aabef56e953dd9a9a4
shams-ivon <sfurvdiorwen@gmail.com>
2023-09-04 15:36:06 +0600

    files to .gitignore

--- gui.py
+++ gui.py
@@ -7,15 +7,15 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+layout = [[label], [input_box, add_button], [list_box, edit_button]]
 
 window = sg.Window("My To-do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=layout,
                    font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+
     if event == "Add":
         todos = functions.get_todos()
         new_todo = values['todo'] + "\n"
@@ -25,7 +25,7 @@ while True:
 
     elif event == "Edit":
         todo_to_edit = values["todos"][0]
-        new_todo = values["todo"] + "\n"
+        new_todo = values["todo"]
 
         todos = functions.get_todos()
         index = todos.index(todo_to_edit)
