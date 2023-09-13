01eae9bda840ba066b949ba1e01757143196e2f7
shams-ivon <sfurvdiorwen@gmail.com>
2023-09-13 20:10:28 +0600

    Added 'Delete' button and 'Exit' button to the GUI

--- gui.py
+++ gui.py
@@ -7,10 +7,14 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
-layout = [[label], [input_box, add_button], [list_box, edit_button]]
+delete_button = sg.Button("Delete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-do App",
-                   layout=layout,
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, delete_button],
+                           [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
@@ -32,9 +36,20 @@ while True:
         todos[index] = new_todo
         functions.write_todos(todos)
         window["todos"].update(values=todos)
+        window['todo'].update(value="")
 
+    elif event == "Delete":
+        todo_to_delete = values["todos"][0]
+        todos = functions.get_todos()
+        todos.remove(todo_to_delete)
+        functions.write_todos(todos)
+        window["todos"].update(values=todos)
+
+    elif event == "Exit":
+        break
     elif event == "todos":
         window["todo"].update(value=values["todos"][0])
+
     elif event == sg.WIN_CLOSED:
         break
 
